026cd570ed9b536c11a723cfb7ea343c582417f4
Marco Schincaglia <orione-team+sch@xpeppers.com>
2012-09-05 22:57:55 +0200

    Initialized app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testapp::Application.config.secret_token = 'a8447ad26e2896874cf36feb4c1f8815f8d6d7d6e29f4689c6a4fea1fc3a03f653de9f2f01b644ed8f752b36ef52b2d244d0cc05e6711aba0219d44c7c2e70a2'
